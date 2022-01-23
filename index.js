a678f5e85b159803fe228799935e8c6f9d99dc17
Tao Chen <taochensyd@gmail.com>
2022-01-23 13:37:32 +1100

    add 2 get endpoint

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const app = express();
+
+app.get('/api/resume', (req, res) => {
+    res.send('Resume Detail')
+});
+
+app.get('/api/resume/:id', (req, res) => {
+    res.send(`userID is : ${req.params.id}`)
+});
