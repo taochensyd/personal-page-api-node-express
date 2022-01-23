949dcddae937aa17e87c8bd441a8789d37caf8dd
Tao Chen <taochensyd@gmail.com>
2022-01-23 15:26:58 +1100

    add git gitignore file

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+'use strict';
+
 const express = require('express')
 const app = express();
+const fs = require('fs');
 
-app.get('/api/resume', (req, res) => {
-    res.send('Resume Detail')
+fs.readFile('./data/resumeData.json', (err, data) => {
+    if (err) throw err;
+    const resume = JSON.parse(data);
+    console.log(resume);
+});
+
+app.get('/', (req, res) => {
+    res.send('Root')
+});
+
+app.get('/api/resume', async (req, res) => {
+    res.send(await resume)
 });
 
 app.get('/api/resume/:id', (req, res) => {
     res.send(`userID is : ${req.params.id}`)
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
